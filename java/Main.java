c85f5a816ba8b35ed9de550adfa52c066150a61f
dragonrain5761 <dragonrain5761@gmail.com>
2023-11-20 19:45:34 +0900

    자바 연습 11.20

---
+++ java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        int number = 5;
+        System.out.println(number);
+
+        String jeju = "Hello jeju";
+        System.out.println(jeju);
+
+        int add = number + 10;
+        System.out.println(add);
+
+        final int finalNumber = 1;
+        System.out.println(finalNumber);
+//        final = 2;
+//        System.out.println(finalNumber);
+    }
+}
